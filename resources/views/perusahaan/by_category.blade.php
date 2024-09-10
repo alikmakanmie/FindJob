380a0a9a8d7cfe063521939467a0730289946e5f
alikmakanmie <141886910+alikmakanmie@users.noreply.github.com>
2024-09-10 20:51:48 +0700

    update

--- resources/views/perusahaan/by_category.blade.php
+++ resources/views/perusahaan/by_category.blade.php
@@ -4,18 +4,24 @@
 <div class="container bg-white p-4 rounded">
     <h2 class="mb-4">Perusahaan dalam Kategori: {{ $kategori->name }}</h2>
     
-    @foreach($perusahaan as $p)
-        <div class="card mb-3">
-            <div class="card-body">
-                <h5>{{ $p->nama }}</h5>
-                <p>{{ $p->deskripsi }}</p>
-                <a href="{{ route('perusahaan.show', $p->id) }}" class="btn btn-secondary">Lihat Detail</a>
-            </div>
+    @if($perusahaan->isEmpty())
+        <div class="alert alert-info">
+            belum ada perusahaan dalam kategori ini.
         </div>
-    @endforeach
+    @else
+        @foreach($perusahaan as $p)
+            <div class="card mb-3">
+                <div class="card-body">
+                    <h5>{{ $p->nama }}</h5>
+                    <p>{{ $p->deskripsi }}</p>
+                    <a href="{{ route('perusahaan.show', $p->id) }}" class="btn btn-secondary">Lihat Detail</a>
+                </div>
+            </div>
+        @endforeach
 
-    <div class="mt-4">
-        {{ $perusahaan->links() }}
-    </div>
+        <div class="mt-4">
+            {{ $perusahaan->links() }}
+        </div>
+    @endif
 </div>
 @endsection
